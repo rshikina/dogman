712f02e01a960db0bf31b6e0e546cb5a31d632bb
rshikina <rshikina@gmail.com>
2023-08-12 10:59:38 -1000

    delete secret

--- dogMan/settings.py
+++ dogMan/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-qr%da&ucn7bf1zhlj&ed*a$zudj&wn(%3r4#cny&pp0x(rl#vs"
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
