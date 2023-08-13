c320c66dcad5a3c8a2496d7b0c52ae7e477708ac
rshikina <rshikina@gmail.com>
2023-08-12 17:02:00 -1000

    updated url with 'app_name' to fix defaultsite error

--- dogMan/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
-from ../ import views
-
-def goHome(request):
-    return redirect(home)
-
--- landingPage/urls.py
+++ landingPage/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "landingPage"
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
