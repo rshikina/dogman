39b4dff99a6b510b93bf289ac66a2460ade6802a
rshikina <rshikina@gmail.com>
2023-08-12 23:25:05 -1000

    WORKING ALL - SAVE POINT

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from .models import Choice, Question
 from django.views import generic
 
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
